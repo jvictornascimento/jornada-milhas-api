15caf747b3c9315ece5efdf0c63aa10fd8ce575a
jvictornascimento <victojoao789@gmail.com>
2024-07-14 23:43:44 -0300

    init project

---
+++ src/test/java/br/com/jnascimento/jornada_milhas/JornadaMilhasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jnascimento.jornada_milhas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JornadaMilhasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
